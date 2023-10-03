bd4add035b324f3166b776dcf3413a3b324d864f
blaclay <clayblackwelder@gmail.com>
2023-10-03 15:45:12 -0700

    added outline/goals to cli.py

---
+++ lib/cli.py
@@ -0,0 +1,4 @@
+# License Plate lookup
+# License Plate enter information (plate number, make, model, owner name, address)
+# Maybe LP adding function does not require a name or address?
+# Additional options: Search by Owner or by Address
